0ee028a09306ec7255047fc11a230709f38b650a
Nunya <you@example.com>
2024-06-15 18:15:40 -0600

    Add escape hatch

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import pyautogui
+import keyboard
+import os
 import time
 from kinematics import click_at
 import math
 
 # TURN OFF ITEM LABELS BEFORE RUNNING BOT
 
-character_level = 89
-net_gold_per_sale = 14_470
-max_sales = (character_level * 10_000 // net_gold_per_sale) - 2
-run_count = 3
+character_level = 90
+net_gold_per_sale = 20_737
+max_sales = (character_level * 10_000 // net_gold_per_sale) - 1
+run_count = 8
 
 # UNCOMMENT THE FOLLOWING 3 LINES TO CONTINUOUSLY PRINT YOUR CURSOR POSITION
 # while True:
@@ -23,14 +25,14 @@ config = {
   'coords': {
     'sell_button': (952, 1110),
     'character_weapon_slot': (1390, 408),
-    'vendor_weapon_position': (1173, 417),
+    'vendor_weapon_position': (633, 820),
     'inventory_origin': (1360, 800),
     'close_vendor_btn': (1355, 1111), # close menu button
     'open_inventory_btn': (1151, 1297), # click inventory?
     'drop_gold_btn': (1500, 1114), # click gold drop icon
     'confirm_dialog_box': (1154, 860), # confirm gold drop
     'vendor_body': (1280, 569), # click vendor
-    'trade_menu_option': (1355, 337), # click trade option
+    'trade_menu_option': (1274, 367), # click trade option
     'vendor_weapon_tab': (815, 200) # click tab
   }
 }
@@ -64,6 +66,12 @@ def drop_gold():
 def print_progress(run_progress: int):
     progress = math.ceil(100 * run_progress / config['sell_count'])
     print(f'{progress}%')
+
+def abort(e):
+  print("Aborting script!")
+  os._exit(0)
+
+keyboard.on_press_key('esc', abort)
 
 print(f"Selling weapon {max_sales} times between gold drops...")
 
